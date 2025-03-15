4c8ac28494159d1af796cd8f45b087daf960078f
Pema Wangchuk <pemawangchug1986@gmail.com>
2025-03-15 10:56:16 +0600

    add url

--- lib/api.ts
+++ lib/api.ts
@@ -4,9 +4,7 @@ import { IUser } from "@/database/user.model";
 
 import { fetchHandler } from "./handlers/fetch";
 
-const API_BASE_URL =
-	process.env.NEXT_PUBLIC_API_BASE_URL ||
-	"'https://stackoverflow-clone-six-ebon.vercel.app/api'";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const api = {
 	auth: {
 		oAuthSignIn: ({
